321eac5a31893d293d0e31cf78213f092b548909
Josh Wills <jcwills369@gmail.com>
2023-09-13 20:36:50 +0100

    added validations to models, seeds and faker gem

--- app/models/user_review.rb
+++ app/models/user_review.rb
@@ -2,4 +2,6 @@ class UserReview < ApplicationRecord
   belongs_to :reviewer, class_name: 'User'
   belongs_to :reviewed, class_name: 'User'
   belongs_to :swap
+
+  validates :rating, numericality: { only_integer: true, greater_than: 0, less_than_or_equal_to: 5 }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Item.destroy_all
+User.destroy_all
+
+user1 = User.create!({ username: "test", email: 'test@test.com', password: 'password', location: 'testville', swapzi_score: 500000 })
+user2 = User.create!({ username: "Josh", email: 'josh@test.com', password: 'password', location: 'London', swapzi_score: 100 })
+user3 = User.create!({ username: "Bay", email: 'bay@test.com', password: 'password', location: 'Berlin', swapzi_score: 500 })
+user4 = User.create!({ username: "Ben", email: 'ben@test.com', password: 'password', location: 'Taunton', swapzi_score: 200 })
+user5 = User.create!({ username: "James", email: 'james@test.com', password: 'password', location: 'Kent', swapzi_score: 1000 })
+
+categories = ["books", "electronics", "musical instruments", "furniture", "clothes", "household appliances", "sports gear", "decorations"]
+
+100.times do
+  item = Item.new({ name: "#{Faker::Adjective.negative} #{Faker::Appliance.equipment}", description: Faker::Movies::HarryPotter.quote, category: categories.sample, swapzi_points: (100..500).to_a.sample })
+  item.user = [user1, user2, user3, user4, user5].sample
+  item.save!
+end
