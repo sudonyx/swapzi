e62fc3421ff50c60cc3ff66ad3d6f0bcd86f06c0
James Marsh <jjmarsh123@gmail.com>
2023-09-15 13:46:06 +0100

    can't quite get the unfavourite button to work

--- app/controllers/favourites_controller.rb
+++ app/controllers/favourites_controller.rb
@@ -1,2 +1,26 @@
 class FavouritesController < ApplicationController
+    def create
+        item = Item.find(params[:item_id])
+        authorize item
+        favourite = current_user.favourites.build(item: item)
+        authorize favourite
+        if favourite.save
+        flash[:notice] = "Saved as favorite!"
+        else
+        flash[:alert] = "Favorite failed to save."
+        end
+        redirect_to item_path(item)
+    end
+    
+    def destroy
+        item = Item.find(params[:item_id])
+        authorize item
+        favourite = current_user.favourites.find(params[:id])
+        authorize favourite
+        if favourite.destroy
+            flash[:notice] = "Item Unfavourited"
+        else 
+            flash[:notice] = "Error"
+        end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
-
-  resources :items, only: %i[index show new create destroy]
+  resources :items, only: %i[index show new create destroy] do
+    post 'favourite', to: 'favourites#create'
+    delete 'favourite', to: 'favourites#destroy'
+  end
 end
