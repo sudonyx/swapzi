2b9845d0dd6e3be4fe0c0f635b64c73799e7cc43
Benjamin Malin <benjamin.malin047@gmail.com>
2023-09-18 19:06:13 +0100

    added user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,33 @@
 class UsersController < ApplicationController
+  class UsersController < ApplicationController
+    before_action :authenticate_user!, except: [:show]
+    before_action :set_user, only: [:show, :edit, :update]
+
+    def show
+    end
+
+    def edit
+      authorize @user
+    end
+
+    def update
+      authorize @user
+
+      if @user.update(user_profile_params)
+        redirect_to user_profile_path(@user), notice: 'Profile updated successfully.'
+      else
+        render :edit
+      end
+    end
+
+    private
+
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    def user_profile_params
+      params.require(:user).permit(:username, :location, :email, :swapzi_score)
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  get 'user_profiles/show'
+  get 'user_profiles/edit'
+  get 'user_profiles/update'
   devise_for :users
+
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
