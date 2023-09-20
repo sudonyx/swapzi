e27349ad91aba39340a31c8035932a0d118ff5dc
Josh Wills <jcwills369@gmail.com>
2023-09-20 15:00:00 +0100

    cleaned up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+
   # Defines the root path route ("/")
   # root "articles#index"
+
   resources :items, only: %i[index show new create destroy] do
-     post 'favourite', to: 'favourites#create'
-     delete 'favourite', to: 'favourites#destroy'
-      resources :swaps, only: %i[new]
+    resources :favourites, only: %i[create destroy]
+    resources :swaps, only: %i[new]
   end
+
   resources :swaps, only: %i[show create update destroy]
-  resources :favourites, only: %i[index create destroy]
   get "/swaps", to: "swaps#pending"
+
+  resources :favourites, only: %i[index]
 end
