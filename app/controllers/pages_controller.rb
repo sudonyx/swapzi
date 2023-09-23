a8ec5b7f304c0999184b5a8a14e332ea38f8d540
Bay Si <xuebeisi@gmail.com>
2023-09-23 19:23:36 +0200

    improved the landing page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    @swaps = Swap.all.order(created_at: :desc).limit(4)
   end
 end
