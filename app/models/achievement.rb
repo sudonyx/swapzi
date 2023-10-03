7775d23ccb34a9de20473691d17ec2205f6f9409
Benjamin Malin <benjamin.malin047@gmail.com>
2023-10-03 17:57:39 +0100

    added user achievement back end + front end update for dashboard

--- app/models/achievement.rb
+++ app/models/achievement.rb
@@ -2,4 +2,8 @@ class Achievement < ApplicationRecord
   has_many :user_achievements
   has_many :users, through: :user_achievements
   has_one_attached :photo, dependent: :destroy
+
+  def completed_by?(user)
+    user_achievements.exists?(user: user)
+  end
 end
